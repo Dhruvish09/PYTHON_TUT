d55d0998895e4e2c82dc22a4170581e48b59069c
Dhruvish09 <44092907+Dhruvish09@users.noreply.github.com>
2020-12-29 23:46:25 +0530

    Add files via upload

--- Operators/operator_overloadding.py
+++ Operators/operator_overloadding.py
@@ -1,1 +1,22 @@
+class python:
+    def __init__(self,pages):
+        self.pages=pages
+    
+    def __add__(self,other):
+        return self.pages + other.pages
+    
+    def __gt__(self,other):
+        return self.pages > other.pages
+    
+    def __sub__(self,other):
+        return self.pages - other.pages
+class sql:
+    def __init__(self,pages):
+         self.pages=pages
+    
+b1=python(123)
+b2=sql(234)
 
+print(b1+b2)
+print(b1>b2)
+print(b1-b2)
