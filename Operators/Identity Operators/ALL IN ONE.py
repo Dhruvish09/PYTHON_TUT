d0bbcefe3550a096deab41d85f3803a1cfac746b
Dhruvish09 <44092907+Dhruvish09@users.noreply.github.com>
2021-01-29 21:21:02 +0530

    Add files via upload

---
+++ Operators/Identity Operators/ALL IN ONE.py
@@ -0,0 +1,16 @@
+# Examples of Identity operators 
+a1 = 3
+b1 = 3
+a2 = 'GeeksforGeeks'
+b2 = 'GeeksforGeeks'
+a3 = [1,2,3] 
+b3 = [1,2,3] 
+
+
+print(a1 is not b1) 
+
+
+print(a2 is b2) 
+
+# Output is False, since lists are mutable. 
+print(a3 is b3) 
--- Operators/Identity Operators/IS.py
+++ Operators/Identity Operators/IS.py
@@ -1,1 +1,17 @@
+#Returns True if both variables are the same object
 
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+
+# returns True because z is the same object as x
+
+print(x is y)
+
+# returns False because x is not the same object as y, even if they have the same content
+
+print(x == y)
+
+# to demonstrate the difference betweeen "is" and "==": this comparison returns True because x is equal to y
