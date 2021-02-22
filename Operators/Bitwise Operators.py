5accd3ccf414f4cb627f1aa169ae856b44e7419f
Dhruvish09 <44092907+Dhruvish09@users.noreply.github.com>
2021-02-22 23:54:44 +0530

    Add files via upload

---
+++ Operators/Bitwise Operators.py
@@ -0,0 +1,28 @@
+# Examples of Bitwise operators 
+a = 10
+b = 4
+
+# Print bitwise AND operation 
+print('operator:1')
+print(a & b) 
+
+
+# Print bitwise OR operation 
+print('operator:2')
+print(a | b) 
+
+# Print bitwise NOT operation 
+print('operator:3')
+print(~a) 
+
+# print bitwise XOR operation 
+print('operator:4')
+print(a ^ b) 
+
+# print bitwise right shift operation 
+print('operator:5')
+print(a >> 2) 
+
+# print bitwise left shift operation 
+print('operator:6')
+print(a << 2) 
